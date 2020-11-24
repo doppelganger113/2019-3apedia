131342a5dd5cec2db3a51d9c16559b1ea8329c87
Marko Kovacevic <marko.kovacevic@poviolabs.com>
2020-11-24 14:36:55 +0100

    Added movies json structs

--- go/main.go
+++ go/main.go
@@ -1,14 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"go-presentation/internal/introduction/arrays"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
-func main() {
-	numbers := []int{1, 2, 3, 4}
+type MovieData struct {
+	Title                string  `json:"Title"`
+	UsGross              int     `json:"US Gross"`
+	WorldwideGross       int     `json:"Worldwide Gross"`
+	USDVDSales           int     `json:"US DVD Sales"`
+	ProductionBudget     int     `json:"Production Budget"`
+	ReleaseDate          string  `json:"Release Date"`
+	MPAARating           string  `json:"MPAA Rating"`
+	RunningTimeMin       int     `json:"Running Time min"`
+	Distributor          string  `json:"Distributor"`
+	Source               string  `json:"Source"`
+	MajorGenre           string  `json:"Major Genre"`
+	CreativeType         string  `json:"Creative Type"`
+	Director             string  `json:"Director"`
+	RottenTomatoesRating int     `json:"Rotten Tomatoes Rating"`
+	IMDBRating           float64 `json:"IMDBRating"`
+	IMDBVotes            int     `json:"IMDB Votes"`
+}
 
-	fmt.Println(
-		arrays.AddNumbers(numbers, 5, 6, 7),
-	)
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	file, err := os.Open("data/movies.json")
+	check(err)
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	check(err)
+
+	var movies []MovieData
+	err = json.Unmarshal(data, &movies)
+	check(err)
+
+	fmt.Println(movies[0])
 }
